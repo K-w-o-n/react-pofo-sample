41a0a958cd1687f6b73f7f526f8052ce78a628a2
Kwon <144373232+K-w-o-n@users.noreply.github.com>
2024-01-24 01:29:07 +0630

    scroll effect add

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -8,12 +8,14 @@ const Navbar = () => {
       <nav className="navbar">
         <h1 className='nav1'>Kwon</h1>
         <div className="desktopMenu">
-            <Link className="desktopMenuItemList active">Home</Link>
-            <Link className="desktopMenuItemList">About</Link>
-            <Link className="desktopMenuItemList">Projects</Link>
-            <Link className="desktopMenuItemList">Clients</Link>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuItemList active">Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuItemList">About</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuItemList">Projects</Link>
+            <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-90} duration={500} className="desktopMenuItemList">Clients</Link>
         </div>
-        <button className="desktopMenuBtn">
+        <button className="desktopMenuBtn" onClick={() => {
+          document.getElementById('contact').scrollIntoView({behaviour: 'smooth'})
+        }}>
             Contact Me
         </button>
       </nav>
